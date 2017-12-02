4d6f9d2c03818091e21226452c9100aa6f7c5151
kmui2 <kmui2@wisc.edu>
2017-12-01 18:39:52 -0600

    fixed current time url

--- web.py/sqlitedb.py
+++ web.py/sqlitedb.py
@@ -1,6 +1,6 @@
 import web
 
-db = web.database(dbn=‘sqlite’, db=‘auctions.db’)
+db = web.database(dbn='sqlite', db='auctions.db')
 
 ######################BEGIN HELPER METHODS######################
 
@@ -30,10 +30,10 @@ def transaction():
 def getTime():
     # TODO: update the query string to match
     # the correct column and table name in your database
-    query_string = 'select currenttime from Time'
+    query_string = 'select Time from CurrentTime'
     results = query(query_string)
     # alternatively: return results[0]['currenttime']
-    return results[0].currenttime # TODO: update this as well to match the
+    return results[0].Time # TODO: update this as well to match the
                                   # column name
 
 # returns a single item specified by the Item's ID in the database
